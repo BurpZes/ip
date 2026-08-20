a3bb6e9114bfdeb57a8b7f439f1f92ea93cd8d8a
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-08-20 23:17:57 +0800

    Added Exceptions For Commands and Deadline

--- src/main/java/Wally.java
+++ src/main/java/Wally.java
@@ -25,60 +25,93 @@ public class Wally {
         while (true) {
             userInput = myScanner.nextLine().strip();
             System.out.println();
-            if (userInput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+
+            try {
+                // Close chatbot
+                if (userInput.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("-".repeat(50));
+                    break;
+                } 
+
+                // list tasks
+                else if (userInput.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasking_size; i++) {
+                        System.out.println(String.valueOf(i + 1) + ". " + taskings[i]);
+                    }
+                } 
+                
+                // mark command
+                else if (userInput.matches("mark \\d")) {
+                    String temp[] = userInput.split(" ");
+                    if (Integer.parseInt(temp[1]) > tasking_size || Integer.parseInt(temp[1]) < 1) {
+                        System.out.println("Invalid tasking number.");
+                    } else {
+                        taskings[Integer.parseInt(temp[1]) - 1].set_status(true);
+                        System.out.println("The following task has been marked as done:");
+                        System.out.println(taskings[Integer.parseInt(temp[1]) - 1]);
+                    }
+                } 
+                
+                // unmark command
+                else if (userInput.matches("unmark \\d")) {
+                    String temp[] = userInput.split(" ");
+                    if (Integer.parseInt(temp[1]) > tasking_size || Integer.parseInt(temp[1]) < 1) {
+                        System.out.println("Invalid tasking number.");
+                    } else {
+                        taskings[Integer.parseInt(temp[1]) - 1].set_status(false);
+                        System.out.println("The following task has been marked as not done yet:");
+                        System.out.println(taskings[Integer.parseInt(temp[1]) - 1]);
+                    }
+                } 
+                
+                // todo command
+                else if (userInput.matches("todo .*")) {
+                    String temp = userInput.split("todo ")[1];
+                    taskings[tasking_size] = new ToDo(temp);
+                    System.out.println("The following task has been added:");
+                    System.out.println(taskings[tasking_size]);
+                    tasking_size += 1;
+                    System.out.println("Now you have " + tasking_size + " tasks in the list.");
+                } 
+                
+                // deadline command
+                else if (userInput.matches("deadline .*")) {
+                    if (userInput.matches("deadline .* /by .*")) {
+                        String temp[] = userInput.split("deadline ")[1].split(" /by ");
+                        taskings[tasking_size] = new Deadline(temp[0], temp[1]);
+                        System.out.println("The following task has been added:");
+                        System.out.println(taskings[tasking_size]);
+                        tasking_size += 1;
+                        System.out.println("Now you have " + tasking_size + " tasks in the list.");
+                    } else {
+                        throw(new InvalidDeadlineException());
+                    }
+                } 
+                
+                // event command
+                else if (userInput.matches("event .* /from .* /to .*")) {
+                    String temp[] = userInput.split("event ")[1].split(" /from | /to ");
+                    taskings[tasking_size] = new Event(temp[0], temp[1], temp[2]);
+                    System.out.println("The following task has been added:");
+                    System.out.println(taskings[tasking_size]);
+                    tasking_size += 1;
+                    System.out.println("Now you have " + tasking_size + " tasks in the list.");
+                } 
+                
+                // everything else
+                else {
+                    throw(new InvalidCommandException());
+                }
+            } catch (InvalidCommandException e) {
+                System.out.println("Invalid Command Entered!");
+            } catch (InvalidDeadlineException e) {
+                System.out.println("Invalid format for deadline tasks!");
+                System.out.println("Format: deadline <name> /by <date>");
+            } finally {
                 System.out.println("-".repeat(50));
-                break;
-            } else if (userInput.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasking_size; i++) {
-                    System.out.println(String.valueOf(i + 1) + ". " + taskings[i]);
-                }
-            } else if (userInput.matches("mark \\d")) {
-                String temp[] = userInput.split(" ");
-                if (Integer.parseInt(temp[1]) > tasking_size || Integer.parseInt(temp[1]) < 1) {
-                    System.out.println("Invalid tasking number.");
-                } else {
-                    taskings[Integer.parseInt(temp[1]) - 1].set_status(true);
-                    System.out.println("The following task has been marked as done:");
-                    System.out.println(taskings[Integer.parseInt(temp[1]) - 1]);
-                }
-            } else if (userInput.matches("unmark \\d")) {
-                String temp[] = userInput.split(" ");
-                if (Integer.parseInt(temp[1]) > tasking_size || Integer.parseInt(temp[1]) < 1) {
-                    System.out.println("Invalid tasking number.");
-                } else {
-                    taskings[Integer.parseInt(temp[1]) - 1].set_status(false);
-                    System.out.println("The following task has been marked as not done yet:");
-                    System.out.println(taskings[Integer.parseInt(temp[1]) - 1]);
-                }
-            } else if (userInput.matches("todo .*")) {
-                String temp = userInput.split("todo ")[1];
-                taskings[tasking_size] = new ToDo(temp);
-                System.out.println("The following task has been added:");
-                System.out.println(taskings[tasking_size]);
-                tasking_size += 1;
-                System.out.println("Now you have " + tasking_size + " tasks in the list.");
-            } else if (userInput.matches("deadline .* /by .*")) {
-                String temp[] = userInput.split("deadline ")[1].split(" /by ");
-                taskings[tasking_size] = new Deadline(temp[0], temp[1]);
-                System.out.println("The following task has been added:");
-                System.out.println(taskings[tasking_size]);
-                tasking_size += 1;
-                System.out.println("Now you have " + tasking_size + " tasks in the list.");
-            } else if (userInput.matches("event .* /from .* /to .*")) {
-                String temp[] = userInput.split("event ")[1].split(" /from | /to ");
-                taskings[tasking_size] = new Event(temp[0], temp[1], temp[2]);
-                System.out.println("The following task has been added:");
-                System.out.println(taskings[tasking_size]);
-                tasking_size += 1;
-                System.out.println("Now you have " + tasking_size + " tasks in the list.");
-            } else {
-                taskings[tasking_size] = new Task(userInput);
-                tasking_size += 1;
-                System.out.println("added: " + userInput);
             }
-            System.out.println("-".repeat(50));
         }
 
         // Cleanup
