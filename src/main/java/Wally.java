76e4989eb2593aba5aea6dbe2dde28b1e097068c
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-08-20 18:45:21 +0800

    Add user greeting

--- src/main/java/Wally.java
+++ src/main/java/Wally.java
@@ -5,6 +5,12 @@ public class Wally {
             + " \\ \\ /\\ / / _ \\ | |   | |  \\ V / \n"
             + "  \\ V  V / ___ \\| |___| |___| |  \n"
             + "   \\_/\\_/_/   \\_\\_____|_____|_|  \n";
+        System.out.println("-".repeat(50));
         System.out.println(banner);
+        System.out.println("-".repeat(50));
+        System.out.println("Hello! I'm Wally.\nWhat can I do for you?");
+        System.out.println("-".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-".repeat(50));
     }
 }
