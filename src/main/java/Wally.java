ceeab57091b2c485f2494f371514288bb2e64614
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-08-20 19:04:31 +0800

    Added command echo and bye

--- src/main/java/Wally.java
+++ src/main/java/Wally.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Wally {
     public static void main(String[] args) {
         String banner = "__        ___    _     _  __   __\n"
@@ -10,7 +12,19 @@ public class Wally {
         System.out.println("-".repeat(50));
         System.out.println("Hello! I'm Wally.\nWhat can I do for you?");
         System.out.println("-".repeat(50));
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-".repeat(50));
+        Scanner myScanner = new Scanner(System.in);
+        String userInput = "";
+        while (true) {
+            userInput = myScanner.nextLine().strip();
+            System.out.println();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-".repeat(50));
+                break;
+            } else {
+                System.out.println(userInput);
+                System.out.println("-".repeat(50));
+            }
+        }
     }
 }
