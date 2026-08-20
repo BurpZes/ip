3d99ceddf448d41da75cbd66d3b15e37d9972b67
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-08-20 18:39:09 +0800

    Renamed Duke to Wally

---
+++ src/main/java/Wally.java
@@ -0,0 +1,10 @@
+public class Wally {
+    public static void main(String[] args) {
+        String banner = "__        ___    _     _  __   __\n"
+            + "\\ \\      / / \\  | |   | | \\ \\ / /\n"
+            + " \\ \\ /\\ / / _ \\ | |   | |  \\ V / \n"
+            + "  \\ V  V / ___ \\| |___| |___| |  \n"
+            + "   \\_/\\_/_/   \\_\\_____|_____|_|  \n";
+        System.out.println(banner);
+    }
+}
