c4aafb26c1433125f95ceb88b1b52260f70eb4f0
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-08-28 10:50:43 +0800

    Add find command

--- src/main/java/wally/ToDo.java
+++ src/main/java/wally/ToDo.java
@@ -3,12 +3,13 @@ package wally;
 public class ToDo extends Task {
     /**
      * Creates a ToDo object
+     * 
      * @param name String representing the task
      */
     public ToDo(String name) {
         super(name);
     }
-    
+
     @Override
     public String toString() {
         return "[T]" + super.toString();
@@ -16,6 +17,6 @@ public class ToDo extends Task {
 
     @Override
     public String getCommand() {
-        return "ToDo " + super.getCommand();
+        return "todo " + super.getCommand();
     }
 }
