8e0943656f8d885e3cdb8a5329c0967ac8abda0a
BurpZes <215450279+BurpZes@users.noreply.github.com>
2026-09-02 17:36:30 +0800

    Update Save.java

--- src/main/java/wally/Save.java
+++ src/main/java/wally/Save.java
@@ -1,21 +1,23 @@
 package wally;
 
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.stream.Stream;
 
 /**
  * Saves changes to task list and loads task list when chatbot starts
  */
 public class Save {
-    public static String SAVE_FILE_PATH = "./src/main/java/wally/Saves/save.txt";
-    public static Path PATH = Paths.get(SAVE_FILE_PATH);
+    public static String SAVE_FILE_PATH = "/wally/Saves/save.txt";
+    private static Path PATH = Path.of(SAVE_FILE_PATH);
 
     /**
      * Loads the task list stored in Saves/save.txt.
      * If the file does not exist, create one.
+     * 
      * @param tasklist Tasklist object representing the list of Tasks
      */
     public Save(Tasklist tasklist) {
